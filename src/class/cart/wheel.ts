cca98ca1d2ffdd8686db8408139c147702b0e23f
John Bussey <jbussey365@yahoo.com>
2026-04-18 16:09:17 -0400

    potential cleanup

--- src/class/cart/wheel.ts
+++ src/class/cart/wheel.ts
@@ -5,9 +5,9 @@ export default class Wheel extends Circle{
 
     angularVelolcity: number = 0;
     minStaticAngularVelocity = 1;
-    maxStaticBreakTorque = 12;
-    maxKineticBrakeTorque = 8;
-    maxThrottleTorque = 6;
+    maxStaticBrakeTorque = 45;
+    maxKineticBrakeTorque = 30;
+    maxThrottleTorque = 15;
 
     constructor(radius: number, position: Victor, mass: number){
         super(
@@ -17,36 +17,47 @@ export default class Wheel extends Circle{
         )
     }
 
-    get direction(): number {
-        return Math.sign(this.angularVelolcity)
+    get AngluralDirection(): number {
+        return this.angularVelolcity < 0 ? -1 : 1;
     }
 
-    staticBrakeTorque(brake: number): number {
-        return -this.direction * this.maxStaticBreakTorque * brake;
+    get insideStaticAngularVelocity(): boolean {
+        return Math.abs(this.angularVelolcity) < this.minStaticAngularVelocity
     }
 
+    brakeTorque(brake: number): number {
+        const maxBrakeTorque = this.insideStaticAngularVelocity ?
+                               this.maxStaticBrakeTorque :
+                               this.maxKineticBrakeTorque;
 
-    kineticBrakeTorque(brake: number): number {
-        return -this.direction * this.maxKineticBrakeTorque * brake;
+        return -this.AngluralDirection * maxBrakeTorque * brake;
     }
 
     throttleTorque(throttle: number): number {
         return this.maxThrottleTorque * throttle;
     }
 
-    brakeTorqueWins(throttleTorque: number, brakeTorque: number): bool {
-        const oppositeSigns = throttleTorque * brakeTorque < 1;
-        const brakeBigger = Math.abs(brakeTorque) > Math.abs(throttleTorque);
-        return oppositeSigns && brakeBigger;
+    firstNumberWins(firstNumber: number, secondNumber: number): boolean {
+        const oppositeSigns = firstNumber * secondNumber <= 0;
+        const firstBigger = Math.abs(firstNumber) > Math.abs(secondNumber);
+        return oppositeSigns && firstBigger;
     }
 
     update(timeStep: number, throttle: number, brake: number): void {
 
-        const totalTorque = this.throttleTorque(throttle) + 
-                            this.kineticBrakeTorque(brake);
+        const throttleTorque = this.throttleTorque(throttle);
+        const brakeTorque = this.brakeTorque(brake);
+        
+        const brakeTorqueWins = this.firstNumberWins(brakeTorque, throttleTorque)
+        const totalTorque = throttleTorque + brakeTorque;
 
         const angularAccel = totalTorque / this.momentOfInertia;
+        
         this.angularVelolcity += angularAccel * timeStep;
         this.angle += this.angularVelolcity * timeStep;
+
+        if(brakeTorqueWins && this.insideStaticAngularVelocity){
+            this.angularVelolcity = 0;
+        }
     }
 }
