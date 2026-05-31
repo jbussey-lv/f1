f4c9da9c615114e89303dbe0f06847ea13e6a219
John Bussey <jbussey365@yahoo.com>
2026-05-31 11:42:07 -0400

    tweaked zoom

--- src/class/game/view.ts
+++ src/class/game/view.ts
@@ -10,7 +10,7 @@ import { radiansToDegrees } from "../../helper.ts";
 const svgNamespace = "http://www.w3.org/2000/svg";
 
 export default class View{
-    pixelsPerMeter: number = 25;
+    pixelsPerMeter: number = 50;
     meterCenter: Victor = new Victor(0, 0);
     world: World;
     svg: HTMLElement;
